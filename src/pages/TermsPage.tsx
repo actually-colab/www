3e51a478118fe51f35dc57c4d0c1366c6663f3df
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-13 14:28:55 -0500

    Added placeholder privacy and terms pages

---
+++ src/pages/TermsPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+const TermsPage: React.FC = () => {
+  return <div className={css(styles.container)} />;
+};
+
+export default TermsPage;
