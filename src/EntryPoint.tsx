1c1478349d33e7751a953d8a704e24376bcf91bf
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-25 17:59:36 -0500

    Removed login page from entrypoint routes

--- src/EntryPoint.tsx
+++ src/EntryPoint.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Route, Switch, withRouter } from "react-router";
 
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
 import PrivacyPage from "./pages/PrivacyPage";
 import TermsPage from "./pages/TermsPage";
 
 const EntryPoint: React.FC = () => {
   return (
     <Switch>
-      <Route path="/login" component={LoginPage} />
       <Route path="/privacy" component={PrivacyPage} />
       <Route path="/terms" component={TermsPage} />
       <Route path="/" component={LandingPage} />
