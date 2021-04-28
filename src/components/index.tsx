9e25fab7ec2d7c2616a7756209704fb5dba650a0
Bailey Tincher <btincher99@gmail.com>
2021-04-27 22:32:11 -0500

    Remove google sign in component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,6 @@
 import Footer from "./Footer";
-import GoogleSignInButton from "./GoogleSignInButton";
 import GradientBorderContainer from "./GradientBorderContainer";
 import GradientText from "./GradientText";
 import Header from "./Header";
 
-export { Footer, GoogleSignInButton, GradientBorderContainer, GradientText, Header };
+export { Footer, GradientBorderContainer, GradientText, Header };
